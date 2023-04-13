c9093065f4f73f53fe05f996298e755906684da3
Devin Bidwell <dbidwell94@biddydev.com>
2023-04-13 01:23:25 -0700

    Add derive macro behind a feature flag

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod sim_connect;
+#[cfg(derive)]
 pub use sim_connect::sim_connect_macros::StructToSimConnect;
 pub use sim_connect::{recv_data, sim_events, sim_units, sim_var_types, sim_vars};
 pub use sim_connect::{SimConnect, SimConnectDatum, StructToSimConnect, ToSimConnect};
